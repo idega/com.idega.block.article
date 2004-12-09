2c9475e5565bea697053f3367e95330988d2b3b0
Joakim Johnsson <joakim@idega.com>
2004-12-09 14:26:19 +0000

    Exception used by ArticleItemBean

---
+++ src/java/com/idega/block/article/bean/ArticleStoreException.java
@@ -0,0 +1,15 @@
+package com.idega.block.article.bean;
+
+
+/**
+ * @author Joakim
+ */
+public class ArticleStoreException extends RuntimeException{
+	public ArticleStoreException() {
+		super();
+	}
+
+	public ArticleStoreException(String s) {
+		super(s);
+	}
+}
