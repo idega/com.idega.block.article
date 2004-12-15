2b2f472c3f4963902d1b5908706a140fd709054c
Joakim Johnsson <joakim@idega.com>
2004-12-15 15:47:16 +0000

    Added user feedback if no article selected when displaying article info

--- src/java/com/idega/block/article/component/ArticleDetailView.java
+++ src/java/com/idega/block/article/component/ArticleDetailView.java
@@ -1,17 +1,20 @@
 /*
- * $Id: ArticleDetailView.java,v 1.3 2004/12/09 14:42:15 joakim Exp $
- *
+ * $Id: ArticleDetailView.java,v 1.4 2004/12/15 15:47:16 joakim Exp $
+ * 
  * Copyright (C) 2004 Idega. All Rights Reserved.
- *
- * This software is the proprietary information of Idega.
- * Use is subject to license terms.
+ * 
+ * This software is the proprietary information of Idega. Use is subject to
+ * license terms.
  */
 package com.idega.block.article.component;
 
+import java.io.IOException;
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlOutputText;
 import javax.faces.component.html.HtmlPanelGrid;
+import javax.faces.context.FacesContext;
 import com.idega.presentation.IWBaseComponent;
+import com.idega.webface.WFComponentSelector;
 import com.idega.webface.WFPage;
 import com.idega.webface.WFPanelUtil;
 import com.idega.webface.WFPlainOutputText;
@@ -20,12 +23,21 @@ import com.idega.webface.convert.WFCommaSeparatedListConverter;
 import com.idega.webface.test.bean.ManagedContentBeans;
 
 /**
- * Last modified: $Date: 2004/12/09 14:42:15 $ by $Author: joakim $
- *
+ * Last modified: $Date: 2004/12/15 15:47:16 $ by $Author: joakim $
+ * 
  * @author Joakim
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class ArticleDetailView extends IWBaseComponent implements ManagedContentBeans {
+
+	private final static String P = "article_detail_view_"; // Id prefix
+
+	private final static String COMPONENT_SELECTOR_ID = P + "component_selector";
+	private final static String NO_ARTICLE_ID = P + "no_article";
+	private final static String ARTICLE_LIST_ID = P + "article_list";
+
+	private final static String ref = ARTICLE_ITEM_BEAN_ID + ".";
+	private final static String bref = WFPage.CONTENT_BUNDLE + ".";
 	
 	public ArticleDetailView() {
 	}
@@ -38,10 +50,23 @@ public class ArticleDetailView extends IWBaseComponent implements ManagedContent
 	 * Creates a preview container for the article.
 	 */
 	private UIComponent getDetailPanel() {
-		String ref = ARTICLE_ITEM_BEAN_ID + ".";
-		String bref = WFPage.CONTENT_BUNDLE + ".";
-
+		HtmlPanelGrid dp = WFPanelUtil.getPlainFormPanel(1);
+		WFComponentSelector cs = new WFComponentSelector();
+		cs.setId(COMPONENT_SELECTOR_ID);
+		String headline = WFUtil.getTextVB(ref + "headline").getTitle();
+		System.out.println("Headline:'" + WFUtil.getTextVB(ref + "headline").getTitle() + "'");
 		HtmlPanelGrid p = WFPanelUtil.getPlainFormPanel(1);
+		p.setId(NO_ARTICLE_ID);
+		p.getChildren().add(WFUtil.getHeaderTextVB(bref + "no_article_selected"));
+		
+		cs.add(p);
+		
+		p = WFPanelUtil.getPlainFormPanel(1);
+		p.setId(ARTICLE_LIST_ID);
+		//		System.out.println("Headline:"+WFUtil.getTextVB(ref +
+		// "headline").getTitle());
+		//		System.out.println("Headline length:"+WFUtil.getTextVB(ref +
+		// "headline").getTitle().length());
 		p.getChildren().add(WFUtil.getHeaderTextVB(ref + "headline"));
 		p.getChildren().add(WFUtil.getText(" "));
 		p.getChildren().add(WFUtil.getTextVB(ref + "teaser"));
@@ -49,9 +74,9 @@ public class ArticleDetailView extends IWBaseComponent implements ManagedContent
 		WFPlainOutputText bodyText = new WFPlainOutputText();
 		WFUtil.setValueBinding(bodyText, "value", ref + "body");
 		p.getChildren().add(bodyText);
-		p.getChildren().add(WFUtil.getBreak());		
+		p.getChildren().add(WFUtil.getBreak());
 		p.getChildren().add(new WFPlainOutputText("<hr/>"));
-		UIComponent g = WFUtil.group(WFUtil.getHeaderTextVB(bref + "author"), WFUtil.getHeaderText(": ")); 
+		UIComponent g = WFUtil.group(WFUtil.getHeaderTextVB(bref + "author"), WFUtil.getHeaderText(": "));
 		g.getChildren().add(WFUtil.getTextVB(ref + "author"));
 		p.getChildren().add(g);
 		p.getChildren().add(WFUtil.getText(" "));
@@ -71,7 +96,7 @@ public class ArticleDetailView extends IWBaseComponent implements ManagedContent
 		p.getChildren().add(WFUtil.getText(" "));
 		g = WFUtil.group(WFUtil.getHeaderTextVB(bref + "current_version"), WFUtil.getHeaderText(": "));
 		g.getChildren().add(WFUtil.getTextVB(ref + "versionId"));
-//		g.getChildren().add(WFUtil.getText("1.5"));
+		//		g.getChildren().add(WFUtil.getText("1.5"));
 		p.getChildren().add(g);
 		p.getChildren().add(WFUtil.getText(" "));
 		g = WFUtil.group(WFUtil.getHeaderTextVB(bref + "comment"), WFUtil.getHeaderText(": "));
@@ -82,7 +107,39 @@ public class ArticleDetailView extends IWBaseComponent implements ManagedContent
 		g.getChildren().add(WFUtil.getTextVB(ref + "source"));
 		p.getChildren().add(g);
 		p.getChildren().add(WFUtil.getText(" "));
-				
-		return p;
+
+		cs.add(p);
+		cs.setSelectedId(NO_ARTICLE_ID, true);
+		dp.getChildren().add(cs);
+		return dp;
+	}
+	
+	private void selectComponent() {
+		WFComponentSelector cs = (WFComponentSelector) findComponent(COMPONENT_SELECTOR_ID);
+
+		String headline = WFUtil.getStringValue(ARTICLE_ITEM_BEAN_ID, "headline");
+		if (cs != null) {
+			if (headline == null || headline.length() == 0) {
+				cs.setSelectedId(NO_ARTICLE_ID, true);
+				cs.setSelectedId(ARTICLE_LIST_ID, false);
+			}
+			else {
+				cs.setSelectedId(NO_ARTICLE_ID, false);
+				cs.setSelectedId(ARTICLE_LIST_ID, true);
+			}
+		}
+		else {
+			cs.setSelectedId(NO_ARTICLE_ID, false);
+			cs.setSelectedId(ARTICLE_LIST_ID, true);
+			System.out.println("Could not find the Component selector!!!");
+		}
+	}
+
+	/**
+	 * @see javax.faces.component.UIComponent#encodeBegin(javax.faces.context.FacesContext)
+	 */
+	public void encodeBegin(FacesContext context) throws IOException {
+		super.encodeBegin(context);
+		selectComponent();
 	}
 }
--- src/java/com/idega/block/article/component/ArticleItemView.java
+++ src/java/com/idega/block/article/component/ArticleItemView.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ArticleItemView.java,v 1.2 2004/12/09 14:42:15 joakim Exp $
+ * $Id: ArticleItemView.java,v 1.3 2004/12/15 15:47:16 joakim Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -8,19 +8,31 @@
  */
 package com.idega.block.article.component;
 
+import java.io.IOException;
 import javax.faces.component.UIComponent;
+import javax.faces.component.html.HtmlPanelGrid;
+import javax.faces.context.FacesContext;
 import com.idega.presentation.IWBaseComponent;
+import com.idega.webface.WFComponentSelector;
 import com.idega.webface.WFContainer;
+import com.idega.webface.WFPage;
+import com.idega.webface.WFPanelUtil;
 import com.idega.webface.WFUtil;
 import com.idega.webface.test.bean.ManagedContentBeans;
 
 /**
- * Last modified: $Date: 2004/12/09 14:42:15 $ by $Author: joakim $
+ * Last modified: $Date: 2004/12/15 15:47:16 $ by $Author: joakim $
  *
  * @author Joakim
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ArticleItemView extends IWBaseComponent implements ManagedContentBeans {
+
+	private final static String P = "article_item_view_"; // Id prefix
+
+	private final static String COMPONENT_SELECTOR_ID = P + "component_selector";
+	private final static String NO_ARTICLE_ID = P + "no_article";
+	private final static String ARTICLE_VIEW_ID = P + "article_view";
 
 	public ArticleItemView() {
 	}
@@ -34,8 +46,18 @@ public class ArticleItemView extends IWBaseComponent implements ManagedContentBe
 	 */
 	private UIComponent getPreviewPanel() {
 		String ref = ARTICLE_ITEM_BEAN_ID + ".";
+		String bref = WFPage.CONTENT_BUNDLE + ".";
+
+		WFComponentSelector cs = new WFComponentSelector();
+		cs.setId(COMPONENT_SELECTOR_ID);
+		
+		HtmlPanelGrid p = WFPanelUtil.getPlainFormPanel(1);
+		p.setId(NO_ARTICLE_ID);
+		p.getChildren().add(WFUtil.getHeaderTextVB(bref + "no_article_selected"));
+		cs.add(p);
 
 		WFContainer c = new WFContainer();
+		c.setId(ARTICLE_VIEW_ID);
 		c.add(WFUtil.getTextVB(ref + "author"));
 		c.add(WFUtil.getText(" : "));
 		c.add(WFUtil.getTextVB(ref + "creationDate"));
@@ -44,6 +66,36 @@ public class ArticleItemView extends IWBaseComponent implements ManagedContentBe
 		c.add(WFUtil.getBreak(2));
 		c.add(WFUtil.getTextVB(ref + "body"));
 		c.add(WFUtil.getBreak(1));
-		return c;
+		cs.add(c);
+
+		cs.setSelectedId(NO_ARTICLE_ID, true);
+		return cs;
+	}
+
+	private void selectComponent() {
+		WFComponentSelector cs = (WFComponentSelector) findComponent(COMPONENT_SELECTOR_ID);
+
+		String headline = WFUtil.getStringValue(ARTICLE_ITEM_BEAN_ID, "headline");
+		if (cs != null) {
+			if (headline == null || headline.length() == 0) {
+				cs.setSelectedId(NO_ARTICLE_ID, true);
+				cs.setSelectedId(ARTICLE_VIEW_ID, false);
+			}else {
+				cs.setSelectedId(NO_ARTICLE_ID, false);
+				cs.setSelectedId(ARTICLE_VIEW_ID, true);
+			}
+		}else {
+			cs.setSelectedId(NO_ARTICLE_ID, false);
+			cs.setSelectedId(ARTICLE_VIEW_ID, true);
+			System.out.println("Could not find the Component selector!!!");
+		}
+	}
+
+	/**
+	 * @see javax.faces.component.UIComponent#encodeBegin(javax.faces.context.FacesContext)
+	 */
+	public void encodeBegin(FacesContext context) throws IOException {
+		super.encodeBegin(context);
+		selectComponent();
 	}
 }
