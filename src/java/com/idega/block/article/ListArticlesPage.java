fbafb6d054cba62b214468bb662c5ad68c9a36e2
Joakim Johnsson <joakim@idega.com>
2004-12-17 14:22:06 +0000

    Removed bref and import

--- src/java/com/idega/block/article/ListArticlesPage.java
+++ src/java/com/idega/block/article/ListArticlesPage.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ListArticlesPage.java,v 1.2 2004/12/03 14:43:31 joakim Exp $
+ * $Id: ListArticlesPage.java,v 1.3 2004/12/17 14:22:06 joakim Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -11,19 +11,17 @@ package com.idega.block.article;
 
 import javax.faces.component.UIComponent;
 import javax.faces.component.html.HtmlPanelGrid;
-
 import com.idega.block.article.component.ListArticlesBlock;
-import com.idega.webface.WFPage;
 import com.idega.webface.WFPanelUtil;
 import com.idega.webface.WFUtil;
 
 /**
  * Search article test/demo page. 
  * <p>
- * Last modified: $Date: 2004/12/03 14:43:31 $ by $Author: joakim $
+ * Last modified: $Date: 2004/12/17 14:22:06 $ by $Author: joakim $
  *
  * @author Anders Lindman
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ListArticlesPage extends CMSPage {
 
@@ -39,7 +37,6 @@ public class ListArticlesPage extends CMSPage {
 	 * Returns a list articles block.
 	 */
 	protected UIComponent getListArticlesBlock() {
-		String bref = WFPage.CONTENT_BUNDLE + ".";
 		HtmlPanelGrid ap = WFPanelUtil.getApplicationPanel();
 		ap.getChildren().add(getFunctionBlock());
 		ListArticlesBlock block = new ListArticlesBlock();
