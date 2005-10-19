ba3b2c9df878e8b6c609a14eeebb33c096ed7b30
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-10-19 18:22:12 +0000

    Removing search from menu

--- src/java/com/idega/block/article/IWBundleStarter.java
+++ src/java/com/idega/block/article/IWBundleStarter.java
@@ -1,5 +1,5 @@
 /*
- * $Id: IWBundleStarter.java,v 1.16 2005/08/11 18:54:57 tryggvil Exp $
+ * $Id: IWBundleStarter.java,v 1.17 2005/10/19 18:22:12 tryggvil Exp $
  * Created on 2.11.2004
  *
  * Copyright (C) 2004 Idega Software hf. All Rights Reserved.
@@ -22,10 +22,10 @@ import com.idega.idegaweb.include.GlobalIncludeManager;
 
 /**
  * 
- *  Last modified: $Date: 2005/08/11 18:54:57 $ by $Author: tryggvil $
+ *  Last modified: $Date: 2005/10/19 18:22:12 $ by $Author: tryggvil $
  * 
  * @author <a href="mailto:tryggvil@idega.com">Tryggvi Larusson</a>
- * @version $Revision: 1.16 $
+ * @version $Revision: 1.17 $
  */
 public class IWBundleStarter implements IWBundleStartable {
 	private static final String STYLE_SHEET_URL = "/style/article.css";
@@ -97,5 +97,6 @@ public class IWBundleStarter implements IWBundleStartable {
 		DefaultViewNode searchArticlesNode = new DefaultViewNode("search",articleNode);
 		searchArticlesNode.setJspUri(bundle.getJSPURI("searcharticle.jsp"));
 		searchArticlesNode.setName("#{localizedStrings['com.idega.block.article']['search_articles']}");
+		searchArticlesNode.setVisibleInMenus(false);
 	}
 }
