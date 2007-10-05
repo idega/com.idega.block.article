ca0a25744a0253c64265fed67df748d61f3e5c4c
Valdas Zemaitis <valdas@idega.com>
2007-10-05 08:36:35 +0000

    Now getting real value of ArticleItemViewer - not hard-coded

--- src/java/com/idega/block/article/business/ArticleConstants.java
+++ src/java/com/idega/block/article/business/ArticleConstants.java
@@ -1,7 +1,10 @@
 package com.idega.block.article.business;
 
+import com.idega.block.article.component.ArticleItemViewer;
+import com.idega.business.SpringBeanName;
 import com.idega.util.CoreConstants;
 
+@SpringBeanName("articleConstants")
 public class ArticleConstants {
 	
 	public static final String IW_BUNDLE_IDENTIFIER = "com.idega.block.article";
@@ -11,5 +14,10 @@ public class ArticleConstants {
 	public static final String EMPTY = CoreConstants.EMPTY;
 	public static final String SPACE = CoreConstants.SPACE;
 	public static final String SLASH = CoreConstants.SLASH;
+	public static final Class<ArticleItemViewer> ARTICLE_ITEM_VIEWER = ArticleItemViewer.class;
 
+	public static Class<ArticleItemViewer> getArticleItemViewerClass() {
+		return ARTICLE_ITEM_VIEWER;
+	}
+	
 }
